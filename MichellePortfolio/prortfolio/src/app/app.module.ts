94b88780f7c29ff8379b000eb8e784ac39c8e232
orlesianroyalty <rmb0047@auburn.edu>
2019-08-11 21:30:54 -0500

    Working carousel

--- MichellePortfolio/prortfolio/src/app/app.module.ts
+++ MichellePortfolio/prortfolio/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-
+import { CarouselModule, WavesModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
 import { MatToolbarModule } from '@angular/material';
 import { ProjectlistComponent } from './projectlist/projectlist.component';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 const paths: Routes = [
   {path: 'home', component: HomeComponent},
@@ -22,9 +22,11 @@ const paths: Routes = [
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MatToolbarModule,
     RouterModule.forRoot(paths),
-    MDBBootstrapModule.forRoot()
+    CarouselModule.forRoot(),
+    WavesModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
