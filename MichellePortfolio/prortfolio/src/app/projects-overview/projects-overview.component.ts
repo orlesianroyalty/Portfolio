05481421d79de9eea1a31fe82fc97fb4ae7fb91d
Rebeca <rmb0047@auburn.edu>
2019-08-14 13:16:29 -0500

    Fade in

--- MichellePortfolio/prortfolio/src/app/projects-overview/projects-overview.component.ts
+++ MichellePortfolio/prortfolio/src/app/projects-overview/projects-overview.component.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { trigger, state, style, animation, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-projects-overview',
   templateUrl: './projects-overview.component.html',
   styleUrls: ['./projects-overview.component.scss'],
+  animations: [
+    trigger('simpleFadeAnimation', [
+      state('in', style({opacity:1})),
+      transition(':enter', [
+        style({opacity:0}),
+        animate(1000)
+      ]),
+      transition(':leave', [
+        animate(1000, style({opacity:0}))
+      ])
+    ])
+  ]
 })
 export class ProjectsOverviewComponent implements OnInit {
 
