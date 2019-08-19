cf23e6074a0a01715e0916aa1908e6ebe25d4393
Rebeca <rmb0047@auburn.edu>
2019-08-19 15:03:13 -0500

    Deleted the extras

--- MichellePortfolio/prortfolio/src/app/app.module.ts
+++ MichellePortfolio/prortfolio/src/app/app.module.ts
@@ -5,41 +5,23 @@ import { AppComponent } from './app.component';
 import { MatToolbarModule } from '@angular/material';
 import { ProjectlistComponent } from './projectlist/projectlist.component';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { ProjectsOverviewComponent } from './projects-overview/projects-overview.component';
-import { TheForestComponent } from './the-forest/the-forest.component';
-import { TigerbookComponent } from './tigerbook/tigerbook.component';
-import { DracarysComponent } from './dracarys/dracarys.component';
-import { SnakesladdersComponent } from './snakesladders/snakesladders.component';
-import { DblockscreenComponent } from './dblockscreen/dblockscreen.component';
 import { IconsModule } from 'angular-bootstrap-md'
 
 const paths: Routes = [
-  {path: 'home', component: HomeComponent},
   {path: '', redirectTo:'/project-list', pathMatch: 'full', data: {state: 'overview'}},
   {path: 'project-overview', component: ProjectsOverviewComponent, data: {state: 'overview'}},
   {path: 'project-list', component: ProjectlistComponent},
-  {path: 'the-forest', component: TheForestComponent},
-  {path: 'dracarys', component: DracarysComponent},
-  {path: 'snakesladders', component: SnakesladdersComponent},
-  {path: 'tigerbook', component: TigerbookComponent},
-  {path: 'dblockscreen', component: DblockscreenComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectlistComponent,
-    HomeComponent,
     ProjectsOverviewComponent,
-    TheForestComponent,
-    TigerbookComponent,
-    DracarysComponent,
-    SnakesladdersComponent,
-    DblockscreenComponent
   ],
   imports: [
     BrowserModule,
