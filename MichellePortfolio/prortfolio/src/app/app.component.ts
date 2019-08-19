7ccb6cbaf59bd3494bea7e7d0e902fee0ff47719
Rebeca <rmb0047@auburn.edu>
2019-08-19 15:12:04 -0500

    Fade in

--- MichellePortfolio/prortfolio/src/app/app.component.ts
+++ MichellePortfolio/prortfolio/src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { ProjectlistComponent } from './projectlist/projectlist.component';
 import { routerTransition } from './animations';
+import { trigger, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [routerTransition]
+  animations: [
+    trigger('startCarousel', [
+      transition(':enter', [
+        style({opacity: 0}),
+        animate(1200)
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'prortfolio';
